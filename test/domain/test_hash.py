e6d6f14405531a130cab2b721e4e7253888c3600
Fernando Pimenta <108306295+fsoupimenta@users.noreply.github.com>
2023-07-04 21:45:19 -0300

    Create unit test for `insert_key`

--- test/domain/test_hash.py
+++ test/domain/test_hash.py
@@ -5,6 +5,14 @@ from src.domain.hash import Hash
 
 class TestHash(unittest.TestCase):
 
+    def setUp(self):
+        self.hash_object = Hash()
+
     def test_should_return_key_mod(self):
-        hash_object = Hash()
-        self.assertEqual(hash_object.hash_function(3), 3, "Mod of key")
+        self.assertEqual(self.hash_object.hash_function(24), 1, "Wrong mod key")
+
+    def test_should_insert_key_on_address(self):
+        self.assertEqual(self.hash_object.insert_key(24, 'Element'), 1, "Wrong address")
+
+    def test_should_insert_key_on_next_address(self):
+        self.assertEqual(self.hash_object.insert_key(48, 'Element'), 2, "Wrong address")
